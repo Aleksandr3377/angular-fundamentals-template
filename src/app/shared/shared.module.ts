59a90016ce63f20259c69488fbf6c134bca81607
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-04-23 22:54:58 +0300

    refactor: clean up module declarations and improve formatting

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,8 +11,8 @@ import {
   LoginFormComponent,
   RegistrationFormComponent,
   CourseFormComponent
-} from "./components";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+} from './components';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DurationPipe } from './pipes/duration.pipe';
 import { CustomDatePipe } from './pipes/custom-date.pipe';
 import { EmailValidatorDirective } from '@shared/directives/email.directive';
@@ -33,13 +33,13 @@ const components = [
 ];
 
 @NgModule({
-  declarations: [components],
+  declarations: [...components],
   imports: [
     CommonModule,
     FontAwesomeModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
-  exports: [components]
+  exports: [...components]
 })
-export class SharedModule { }
+export class SharedModule {}
