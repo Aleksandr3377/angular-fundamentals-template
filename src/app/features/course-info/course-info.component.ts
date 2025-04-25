afa8f2031997d7350eace619b69e987dc8a0a5ed
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-04-25 21:05:26 +0300

    feat: add CourseInfoComponent and update tslib version

---
+++ src/app/features/course-info/course-info.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-course-info',
+    template: `<p>Course Info works!</p>`,
+    standalone: true
+})
+export class CourseInfoComponent {}
