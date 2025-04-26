535a1787925a0a3a0d8836a48afea184737e10f9
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-04-26 14:30:46 +0300

    test: simplify HeaderComponent tests by removing unnecessary imports

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -3,18 +3,16 @@ import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
     it('should create', async () => {
-        const view = await render(HeaderComponent, {
-            imports: [],
-        });
+        const view = await render(HeaderComponent);
         expect(view.fixture.componentInstance).toBeTruthy();
     });
 
     it('should project content on the right side', async () => {
         const view = await render(`
-      <app-header>
-        <span>Right Side Content</span>
-      </app-header>
-    `, {
+            <app-header>
+                <span>Right Side Content</span>
+            </app-header>
+        `, {
             imports: [HeaderComponent],
         });
 
@@ -23,11 +21,11 @@ describe('HeaderComponent', () => {
 
     it('should project buttons on the right side', async () => {
         const view = await render(`
-      <app-header>
-        <button>Button 1</button>
-        <button>Button 2</button>
-      </app-header>
-    `, {
+            <app-header>
+                <button>Button 1</button>
+                <button>Button 2</button>
+            </app-header>
+        `, {
             imports: [HeaderComponent],
         });
 
