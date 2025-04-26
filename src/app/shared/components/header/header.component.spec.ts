863ac9568efc7370a4d7975702fec57c3de9edc0
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-04-26 13:05:58 +0300

    feat: add @testing-library/angular dependency

---
+++ src/app/shared/components/header/header.component.spec.ts
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/angular';
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+    it('should create', async () => {
+        const component = await render(HeaderComponent);
+        expect(component.fixture.componentInstance).toBeTruthy();
+    });
+
+    it('should project content on the right side', async () => {
+        const view = await render(`
+      <app-header>
+        <span>Right Side Content</span>
+      </app-header>
+    `, {
+            imports: [HeaderComponent]
+        });
+
+        expect(view.getByText('Right Side Content')).toBeTruthy();
+    });
+
+    it('should project buttons on the right side', async () => {
+        const view = await render(`
+      <app-header>
+        <button>Button 1</button>
+        <button>Button 2</button>
+      </app-header>
+    `, {
+            imports: [HeaderComponent]
+        });
+
+        expect(view.getByText('Button 1')).toBeTruthy();
+        expect(view.getByText('Button 2')).toBeTruthy();
+    });
+});
