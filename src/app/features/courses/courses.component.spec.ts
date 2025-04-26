ea98b371eec1c020a6a089e7c2b438fe7b943aaf
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-04-26 14:13:43 +0300

    test: add unit tests for CoursesComponent and update HeaderComponent tests

---
+++ src/app/features/courses/courses.component.spec.ts
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/angular';
+import { CoursesComponent } from './courses.component';
+
+describe('CoursesComponent', () => {
+    it('should create', async () => {
+        const view = await render(CoursesComponent);
+        expect(view.fixture.componentInstance).toBeTruthy();
+    });
+
+    it('should render content', async () => {
+        const view = await render(CoursesComponent);
+        expect(view.getByText('Courses')).toBeTruthy();
+    });
+});
--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -4,18 +4,18 @@ import { HeaderComponent } from './header.component';
 describe('HeaderComponent', () => {
     it('should create', async () => {
         const view = await render(HeaderComponent, {
-            imports: []
+            imports: [],
         });
         expect(view.fixture.componentInstance).toBeTruthy();
     });
 
     it('should project content on the right side', async () => {
         const view = await render(`
-          <app-header>
-            <span>Right Side Content</span>
-          </app-header>
-        `, {
-            imports: [HeaderComponent] 
+      <app-header>
+        <span>Right Side Content</span>
+      </app-header>
+    `, {
+            imports: [HeaderComponent],
         });
 
         expect(view.getByText('Right Side Content')).toBeTruthy();
@@ -23,12 +23,12 @@ describe('HeaderComponent', () => {
 
     it('should project buttons on the right side', async () => {
         const view = await render(`
-          <app-header>
-            <button>Button 1</button>
-            <button>Button 2</button>
-          </app-header>
-        `, {
-            imports: [HeaderComponent]
+      <app-header>
+        <button>Button 1</button>
+        <button>Button 2</button>
+      </app-header>
+    `, {
+            imports: [HeaderComponent],
         });
 
         expect(view.getByText('Button 1')).toBeTruthy();
