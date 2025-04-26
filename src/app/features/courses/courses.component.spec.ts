9193afb38da107947fdeec2dc9c2bd730010ef80
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-04-26 14:48:27 +0300

    test: update CoursesComponent and HeaderComponent tests to use new rendering approach

--- src/app/features/courses/courses.component.spec.ts
+++ src/app/features/courses/courses.component.spec.ts
@@ -3,12 +3,16 @@ import { CoursesComponent } from './courses.component';
 
 describe('CoursesComponent', () => {
     it('should create', async () => {
-        const view = await render(CoursesComponent);
+        const view = await render(CoursesComponent, {
+            imports: [CoursesComponent],
+        });
         expect(view.fixture.componentInstance).toBeTruthy();
     });
 
     it('should render content', async () => {
-        const view = await render(CoursesComponent);
+        const view = await render(CoursesComponent, {
+            imports: [CoursesComponent],
+        });
         expect(view.getByText('Courses')).toBeTruthy();
     });
 });
