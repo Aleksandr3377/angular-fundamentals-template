9648318f3b247e140cdecc84f57a11b0808ce490
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-04-28 12:52:51 +0300

    feat: add custom email validator directive and integrate it into login form

--- login-form.component.ts
+++ login-form.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {EmailValidatorDirective} from "@app/login-form/email-validator.directive";
 
 @Component({
     selector: 'app-login-form',
     standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        EmailValidatorDirective
+    ],
     templateUrl: './login-form.component.html'
 })
 export class LoginFormComponent {
---
+++ src/app/login-form/email-validator.directive.ts
@@ -0,0 +1,19 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+@Directive({
+    selector: '[appEmailValidator]',
+    standalone: true,
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: EmailValidatorDirective,
+        multi: true
+    }]
+})
+export class EmailValidatorDirective implements Validator {
+    validate(control: AbstractControl): ValidationErrors | null {
+        const emailRegexp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        const valid = emailRegexp.test(control.value);
+        return valid ? null : { invalidEmail: true };
+    }
+}
