4bdf2d68b9df9ca88586b5d79e4f07f4b81bc8fe
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-05-02 15:19:49 +0300

    feat: update unit tests in CoursesService to use done callback for asynchronous operations

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,3 +1,4 @@
+
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import {CoursesService} from "@app/services/courses.service";
@@ -11,7 +12,6 @@ describe('CoursesService', () => {
             imports: [HttpClientTestingModule],
             providers: [CoursesService]
         });
-
         service = TestBed.inject(CoursesService);
         httpMock = TestBed.inject(HttpTestingController);
     });
@@ -20,11 +20,12 @@ describe('CoursesService', () => {
         httpMock.verify();
     });
 
-    it('should retrieve all courses', () => {
+    it('should retrieve all courses', (done) => {
         const dummyCourses = [{ id: 1, title: 'Course 1' }, { id: 2, title: 'Course 2' }];
 
-        service.getAll().subscribe((courses: any) => {
+        service.getAll().subscribe((courses) => {
             expect(courses).toEqual(dummyCourses);
+            done();
         });
 
         const req = httpMock.expectOne('http://localhost:4000/api/courses');
@@ -32,11 +33,12 @@ describe('CoursesService', () => {
         req.flush(dummyCourses);
     });
 
-    it('should create a new course', () => {
+    it('should create a new course', (done) => {
         const newCourse = { title: 'New Course' };
 
-        service.createCourse(newCourse).subscribe((res: any) => {
+        service.createCourse(newCourse).subscribe((res) => {
             expect(res).toEqual({ success: true });
+            done();
         });
 
         const req = httpMock.expectOne('http://localhost:4000/api/courses');
@@ -44,12 +46,13 @@ describe('CoursesService', () => {
         req.flush({ success: true });
     });
 
-    it('should retrieve a course by ID', () => {
+    it('should retrieve a course by ID', (done) => {
         const id = '1';
         const dummyCourse = { id, title: 'Course 1' };
 
-        service.getCourse(id).subscribe((course: any) => {
+        service.getCourse(id).subscribe((course) => {
             expect(course).toEqual(dummyCourse);
+            done();
         });
 
         const req = httpMock.expectOne(`http://localhost:4000/api/courses/${id}`);
@@ -57,12 +60,13 @@ describe('CoursesService', () => {
         req.flush(dummyCourse);
     });
 
-    it('should update a course', () => {
+    it('should update a course', (done) => {
         const id = '1';
         const updatedCourse = { title: 'Updated Course' };
 
-        service.editCourse(id, updatedCourse).subscribe((res: any) => {
+        service.editCourse(id, updatedCourse).subscribe((res) => {
             expect(res).toEqual({ success: true });
+            done();
         });
 
         const req = httpMock.expectOne(`http://localhost:4000/api/courses/${id}`);
@@ -70,11 +74,12 @@ describe('CoursesService', () => {
         req.flush({ success: true });
     });
 
-    it('should delete a course', () => {
+    it('should delete a course', (done) => {
         const id = '1';
 
-        service.deleteCourse(id).subscribe((res: any) => {
+        service.deleteCourse(id).subscribe((res) => {
             expect(res).toEqual({ success: true });
+            done();
         });
 
         const req = httpMock.expectOne(`http://localhost:4000/api/courses/${id}`);
@@ -82,12 +87,13 @@ describe('CoursesService', () => {
         req.flush({ success: true });
     });
 
-    it('should filter courses by query', () => {
+    it('should filter courses by query', (done) => {
         const query = 'angular';
         const filtered = [{ id: 1, title: 'Angular Basics' }];
 
-        service.filterCourses(query).subscribe((res: any) => {
+        service.filterCourses(query).subscribe((res) => {
             expect(res).toEqual(filtered);
+            done();
         });
 
         const req = httpMock.expectOne(`http://localhost:4000/api/courses?filter=${query}`);
@@ -95,11 +101,12 @@ describe('CoursesService', () => {
         req.flush(filtered);
     });
 
-    it('should get all authors', () => {
+    it('should get all authors', (done) => {
         const authors = [{ id: 1, name: 'Author 1' }];
 
-        service.getAllAuthors().subscribe((res: any) => {
+        service.getAllAuthors().subscribe((res) => {
             expect(res).toEqual(authors);
+            done();
         });
 
         const req = httpMock.expectOne('http://localhost:4000/api/authors');
@@ -107,11 +114,12 @@ describe('CoursesService', () => {
         req.flush(authors);
     });
 
-    it('should create a new author', () => {
+    it('should create a new author', (done) => {
         const newAuthor = { name: 'Author A' };
 
-        service.createAuthor(newAuthor).subscribe((res: any) => {
+        service.createAuthor(newAuthor).subscribe((res) => {
             expect(res).toEqual({ success: true });
+            done();
         });
 
         const req = httpMock.expectOne('http://localhost:4000/api/authors');
@@ -119,12 +127,13 @@ describe('CoursesService', () => {
         req.flush({ success: true });
     });
 
-    it('should retrieve author by ID', () => {
+    it('should retrieve author by ID', (done) => {
         const id = '1';
         const author = { id, name: 'Author 1' };
 
-        service.getAuthorById(id).subscribe((res: any) => {
+        service.getAuthorById(id).subscribe((res) => {
             expect(res).toEqual(author);
+            done();
         });
 
         const req = httpMock.expectOne(`http://localhost:4000/api/authors/${id}`);
